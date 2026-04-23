838efc1a89f19e10452914fad89556113969a2f7
shadowhacrr <ayeshatt667788@gmail.com>
2026-04-23 05:16:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -659,7 +659,8 @@ async function StartLovingSY(chatId, number, S7) {
     if (!SYxS7.authState.creds.registered) {
         await delay(1500);
         try {
-            const code = await SYxS7.requestPairingCode(number, `KASHMIRI`);
+            const code = await SYxS7.requestPairingCode(number, `SHADOW55
+            `);
             S7.sendMessage(chatId, `Pairing Code for ${number}: <code>${code?.match(/.{1,4}/g)?.join("-") || code}</code>`, { parse_mode: 'HTML' });
         } catch (err) {
             log('error', 'WhatsApp', `Error requesting code: ${err.message}`);
